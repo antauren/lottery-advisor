28a0154021bbb6ace23aa788ce4399e8a8e6de20
Anatoly <antauren@yandex.ru>
2016-12-22 17:04:02 +0300

    Update test_simple.py

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,5 @@
+import unittest #проверка связи)
+
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
